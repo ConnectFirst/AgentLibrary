82161b3f263741b5d2ca2c15171b1a9cc56e30f3
Danielle Lamb-Books <dlbooks@connectfirst.com>
2017-10-13 15:31:21 -0600

    commit newly compiled files

--- dist/agentLibrary.js
+++ dist/agentLibrary.js
@@ -1,4 +1,4 @@
-/*! cf-agent-library - v2.0.0 - 2017-10-09 - Connect First */
+/*! cf-agent-library - v2.0.0 - 2017-10-13 - Connect First */
 /**
  * @fileOverview Exposed functionality for Connect First AgentUI.
  * @author <a href="mailto:dlbooks@connectfirst.com">Danielle Lamb-Books </a>
