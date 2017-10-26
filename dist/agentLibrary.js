ce5d36a6eff51540c7c52b5f5f674e6389512b29
Andrew Varnell <avarnell@connectfirst.com>
2017-10-26 13:58:46 -0600

    CCI-5439

--- dist/agentLibrary.js
+++ dist/agentLibrary.js
@@ -1,4 +1,4 @@
-/*! cf-agent-library - v2.0.0 - 2017-10-24 - Connect First */
+/*! cf-agent-library - v2.0.0 - 2017-10-26 - Connect First */
 /**
  * @fileOverview Exposed functionality for Connect First AgentUI.
  * @author <a href="mailto:dlbooks@connectfirst.com">Danielle Lamb-Books </a>
