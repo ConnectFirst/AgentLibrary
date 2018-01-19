780c0d180f4cc9593b1488577f826f1b141fba60
Unknown <rmyers@connectfirst.com>
2018-01-19 09:34:03 -0700

    compile after merge

--- dist/agentLibrary.js
+++ dist/agentLibrary.js
@@ -1,4 +1,4 @@
-/*! cf-agent-library - v2.0.0 - 2018-01-18 - Connect First */
+/*! cf-agent-library - v2.0.0 - 2018-01-19 - Connect First */
 /**
  * @fileOverview Exposed functionality for Connect First AgentUI.
  * @author <a href="mailto:dlbooks@connectfirst.com">Danielle Lamb-Books </a>
