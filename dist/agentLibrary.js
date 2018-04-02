cd8152985b7646462bda37e50ce335957abd1804
Ross Myers <rmyers@connectfirst.com>
2018-04-02 15:13:15 -0600

    build after merge

--- dist/agentLibrary.js
+++ dist/agentLibrary.js
@@ -1,4 +1,4 @@
-/*! cf-agent-library - v2.0.0 - 2018-03-28 - Connect First */
+/*! cf-agent-library - v2.0.0 - 2018-04-02 - Connect First */
 /**
  * @fileOverview Exposed functionality for Connect First AgentUI.
  * @author <a href="mailto:dlbooks@connectfirst.com">Danielle Lamb-Books </a>
