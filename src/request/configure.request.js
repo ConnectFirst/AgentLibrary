b5e920191ccafea2118b051eaec2b09a7e31db91
Andrew Varnell <avarnell@connectfirst.com>
2018-09-25 14:17:27 -0600

    CCI-6719 reset pendingDisp

--- src/request/configure.request.js
+++ src/request/configure.request.js
@@ -204,6 +204,7 @@ ConfigRequest.prototype.processResponse = function(response) {
                 model.connectionSettings.activeCallUii  =  utils.getText(resp, "active_call_uii");
                 model.connectionSettings.isPendingDisp = utils.getText(resp, "is_pending_disp");
 
+
                 if(model.connectionSettings.isOnCall === false){
                     if(model.currentCall.uii) {
                         var mockEndCallPacket = {
@@ -232,6 +233,9 @@ ConfigRequest.prototype.processResponse = function(response) {
                         Lib.offhookTerm(agentProcessOffhookCallback);
                     }
                 }else{
+                    //reset pending disp if disp lost.
+                    model.currentCall.pendingDisp = model.connectionSettings.isPendingDisp;
+
                     //agent still is on call and there are transferSessions, verify no transferSession were drop
                     var activeAgentUiSessions = Lib.getTransferSessions();
                     var activeAgentSessions = response.ui_response.active_call_sessions.call_session_id.map(function(sessionObj){
