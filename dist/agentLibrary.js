a296085d1b122f31648dc4201e61d6977df0be49
Ross Myers <rmyers@connectfirst.com>
2018-10-01 09:47:41 -0600

    rebuild

--- dist/agentLibrary.js
+++ dist/agentLibrary.js
@@ -1,4 +1,4 @@
-/*! cf-agent-library - v2.1.10 - 2018-09-26 */
+/*! cf-agent-library - v2.1.10 - 2018-10-01 */
 /**
  * @fileOverview Exposed functionality for Contact Center AgentUI.
  * @version 2.1.8
