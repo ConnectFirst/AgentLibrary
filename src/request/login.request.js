9d5e6ec057420cb9a47298023a8d824bc9ac7c71
Andrew Varnell <avarnell@connectfirst.com>
2018-10-29 13:50:27 -0600

    CCI-6420 pass and set parameter

--- src/request/login.request.js
+++ src/request/login.request.js
@@ -202,6 +202,7 @@ LoginRequest.prototype.processResponse = function(response) {
             model.agentPermissions.allowCrossQueueRequeue = utils.getText(resp, 'allow_cross_gate_requeue') === "1";
             model.agentPermissions.disableSupervisorMonitoring = utils.getText(resp, 'disable_supervisor_monitoring') === "1";
             model.agentPermissions.allowAutoAnswer = utils.getText(resp, 'allow_auto_answer') === "1";
+            model.agentPermissions.enableAutoAnswer = utils.getText(resp, 'enable_auto_answer') === "1";
             model.agentPermissions.allowHistoricalDialing = utils.getText(resp, 'allow_historical_dialing') === "1";
 
             model.outboundSettings.defaultDialGroup = utils.getText(resp, 'phone_login_dial_group');
